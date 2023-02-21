f50b77055e0bddcc862ebb39943f2b2f896a8d47
Ed Page <eopage@gmail.com>
2023-02-21 10:21:15 -0600

    refactor: Use winnow's trace

--- src/parser.rs
+++ src/parser.rs
@@ -7,6 +7,7 @@ use winnow::combinator::{cut_err, eof, fail, opt, peek};
 use winnow::error::{ContextError, ErrMode, ErrorKind, ParseError};
 use winnow::multi::many0;
 use winnow::sequence::{delimited, preceded, terminated};
+use winnow::trace::trace;
 use winnow::IResult;
 use winnow::Parser;
 
@@ -219,38 +220,6 @@ fn exclamation_mark<'a, E: ParseError<&'a str> + ContextError<&'a str> + std::fm
 }
 
 pub(crate) const BREAKER: &str = "exclamation_mark";
-
-#[cfg(feature = "unstable-trace")]
-pub(crate) fn trace<I: std::fmt::Debug, O: std::fmt::Debug, E: std::fmt::Debug>(
-    context: impl std::fmt::Display,
-    mut parser: impl winnow::Parser<I, O, E>,
-) -> impl FnMut(I) -> IResult<I, O, E> {
-    static DEPTH: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
-    move |input: I| {
-        let depth = DEPTH.fetch_add(1, std::sync::atomic::Ordering::SeqCst) * 2;
-        eprintln!("{:depth$}--> {} {:?}", "", context, input);
-        match parser.parse_next(input) {
-            Ok((i, o)) => {
-                DEPTH.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
-                eprintln!("{:depth$}<-- {} {:?}", "", context, i);
-                Ok((i, o))
-            }
-            Err(err) => {
-                DEPTH.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
-                eprintln!("{:depth$}<-- {} {:?}", "", context, err);
-                Err(err)
-            }
-        }
-    }
-}
-
-#[cfg(not(feature = "unstable-trace"))]
-pub(crate) fn trace<I: std::fmt::Debug, O: std::fmt::Debug, E: std::fmt::Debug>(
-    _context: impl std::fmt::Display,
-    mut parser: impl winnow::Parser<I, O, E>,
-) -> impl FnMut(I) -> IResult<I, O, E> {
-    move |input: I| parser.parse_next(input)
-}
 
 #[cfg(test)]
 #[allow(clippy::non_ascii_literal)]
